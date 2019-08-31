96646f80230b79aaa69f6a6938951c5033065d75
Ioannis Poulakas <giannis.p@gmail.com>
2019-08-31 09:15:12 +0300

    Remove unused plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,8 +129,5 @@ module.exports = {
       },
     },
     'gatsby-plugin-sitemap',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
   ],
 };
